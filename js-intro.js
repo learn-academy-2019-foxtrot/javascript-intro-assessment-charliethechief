38b86d61f231bc6a752e4705397789629d034ace
Ubuntu <ubuntu@ip-172-31-35-216.us-east-2.compute.internal>
2019-11-25 17:00:04 +0000

    completed assessment

--- js-intro.js
+++ js-intro.js
@@ -3,19 +3,22 @@
 
 // --------------------------- Consider the following variable:
 
-var mantra = "Be the dev"
+// var mantra = "Be the dev"
 
 // 1a. Write the code that determines if there is a 'B' in mantra.
 
-
+var mantra = "Be the dev";
+console.log(mantra.includes("B"))
 
 // 1b. Write the code that determines if there is an 'x' in mantra.
 
-
+var mantra = "Be the dev";
+console.log(mantra.includes("x"))
 
 // 1c. Write the code that determines if there is a 'v' in mantra.
 
-
+var mantra = "Be the dev";
+console.log(mantra.includes("v"))
 
 // 1d. STRETCH: Write a function that can take two arguments - a string and a letter and returns a sentence telling the user if the letter exists within the string.
 
@@ -23,17 +26,30 @@ var mantra = "Be the dev"
 
 // ------------------------------ Consider the following variables:
 
-var myDog = "Snoopy"
-var myCat = "Garfield"
+// var myDog = "Snoopy"
+// var myCat = "Garfield"
 
 // 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
 
-
-
+var myDog = "Snoopy"
+var myCat = "Garfield"
+var letterChecker = ()=>{
+if (myDog.length > myCat.length){
+    return "snoopy"
+}else{
+    return "garfield"
+    }
+}
+console.log (letterChecker())
 
 // 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
 
-
+var myDog = "Snoopy"
+var myCat = "Garfield"
+var arr1 = (myDog,myCat) => {
+    return [myDog,myCat]
+}
+console.log(arr1(myCat,myDog))
 
 
 // 2c. STRETCH: Write a function that accepts two variables and returns a string in all lowercase letters. Expected output: "snoopy garfield"
@@ -43,15 +59,25 @@ var myCat = "Garfield"
 
 // ------------------------------ Consider the following variable:
 
-var myMessage = "Learning is fun!"
+// var myMessage = "Learning is fun!"
 
 // 3a. Write the code that logs each letter of the message using a for loop.
 //hint: "logs" in this case means "prints to the terminal console."  This is typically done with a console.log(x), where x is the string, function, etc that you wish to see in the terminal.
 
-
+var myMessage = "Learning is fun!"
+const myMessage2 = (string) =>{
+let newVar = myMessage
+for(let i=0; i< myMessage.length; i++){
+}
+return newVar
+}
+    console.log(myMessage2(myMessage))
+    
 // 3b. Write the code that logs each letter of the message using map.
 
-
+var myMessage = "Learning is fun!"
+var newMyMessage = myMessage.split("").map(function(z){return z})
+console.log (myMessage)
 
 // 3c. STRETCH: Write the code that logs each letter of the message using a while loop.
 
@@ -69,7 +95,11 @@ var testString = "thisisateststring"
 // 4a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
 
 
-
+var newString1 = testString.split("")
+const vowler = (x) =>{
+    return x.filter(y =>"aeiou".includes (y.toLowerCase())===false).join("")
+}
+console.log(vowler(newString1))
 
 
 // 4b. STRETCH: Update your function to return an error message if the variable is not a string. Copy and paste your function to add these changes.
@@ -85,12 +115,14 @@ var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "ca
 
 // 5a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
 
-
+var cats1 = toonimals.filter (value => value.animal ==="cat")
+console.log (cats1)
 
 
 //5b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
 
-
+var cats2 = toonimals.filter (value => value.animal !== "cat").map((value) => value.name)
+console.log (cats2)
 
 
 //5c. STRETCH: Create a function that will print a sentence for every toon stating their name and their animal type. Use string interpolation. Expected output: "Itchy is a mouse" "Stimpy is a cat" "Daffy is a duck" "Scratchy is a cat" "Ren is a dog" "Felix is a cat"
--- js-intro.md
+++ js-intro.md
@@ -5,49 +5,51 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: a function is a part of JS code. They are a very useful part of coding because its the easiest way to tell the computer what you want to achieve with your code. 
 
-  Researched answer:
+  Researched answer:A JavaScript function is a block of code designed to perform a particular task.
+
+
 
 
 
 2. List 5 examples of JavaScript array methods.
 
-  Your answer:
+  Your answer: .map, .splice, .split, .toString,.join
 
-  Researched answer:
+  Researched answer: .push, .pop,.shift,.unshift,.concat
 
 
 
 3. Explain the difference between console.log() and return.
 
-  Your answer:
+  Your answer: console.log will call somthing to be shown in terminal while return is a part of a function.
 
-  Researched answer:
-
+  Researched answer:console.log will display the parameter passed to the log method in the console window. Use this method to display a string or variable in the console window
+When using the return statement, the function will stop executing, and return the specified value.
 
 
 4. In regards to functions, what is an argument? What is the difference between an argument and a variable?
 
-  Your answer:
+  Your answer: An argument is what defines the function. An argument can only be inside of a function while a variable can be outside of one.
 
-  Researched answer:
+  Researched answer: The parameters are the aliases for the values that will be passed to the function. The arguments are the actual values.
 
 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer:the driver is the person activley coding while the controller is watching them code and trying to catch syntax and logical errors
 
-  Researched answer:
+  Researched answer:Pair programming is an agile software development technique in which two programmers work together at one workstation. One, the driver, writes code while the other, the observer or navigator, reviews each line of code as it is typed in. The two programmers switch roles frequently.
 
 
 
 6. Give a brief description of JavaScript as if you were explaining it to someone who doesn't know anything about programming.
 
-  Your answer:
+  Your answer: JS is a computer programming language that has many diffrent functions. Most websites have some kind of js in them so it is a good lanuage to know.
 
 
 7. What is one thing we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: live coding with a partner and having a teacher there so I can ask dumb questions. 
